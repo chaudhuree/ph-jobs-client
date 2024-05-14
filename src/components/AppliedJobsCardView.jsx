f06c95d39ef1b0c2a0916c9d8521a819d604b2bd
chaudhuree <chaudhuree00@gmail.com>
2024-05-14 19:04:18 +0600

    fix some design issue

--- src/components/AppliedJobsCardView.jsx
+++ src/components/AppliedJobsCardView.jsx
@@ -3,7 +3,7 @@ import AppliedJobCard from "./AppliedJobCard";
 export default function AppliedJobsCardView({ jobsData }) {
   return (
     <div className="container mx-auto px-5 my-5">
-      <div className="flex flex-wrap gap-6 ">
+      <div className="flex flex-wrap justify-center gap-6 overflow-hidden">
         {jobsData.length === 0 ? (
           <div className="text-center p-5 mx-auto text-xl text-bold text-orange-500">
             no jobs found
