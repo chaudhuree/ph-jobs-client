1f245f22278d1adc4d998dbd0eec4de674aa8c8a
chaudhuree <chaudhuree00@gmail.com>
2024-05-15 23:30:01 +0600

    picture updated for login reg profile and forget pass page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -65,7 +65,7 @@ export default function Register() {
         <title>Sign Up</title>
       </Helmet>
       <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-4xl">
-        <div className="hidden bg-cover lg:block lg:w-1/2 bg-[url('https://images.unsplash.com/photo-1606660265514-358ebbadc80d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80')]"></div>
+        <div className="hidden bg-cover lg:block lg:w-1/2 bg-[url('https://i.ibb.co/HCGZhzd/thom-milkovic-Fp-XTdv4-W2w-unsplash-2.jpg')]"></div>
 
         <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
           <div className="flex justify-center mx-auto -mb-6">
